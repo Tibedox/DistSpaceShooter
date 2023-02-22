a01fac266aa348e0b6d7b0e6da025d220215f592
Tibedox <tibedox@mail.ru>
2023-02-22 22:59:41 +0600

    screenIntro

--- core/src/ru/myitschool/distspaceshooter/MyGG.java
+++ core/src/ru/myitschool/distspaceshooter/MyGG.java
@@ -1,16 +1,13 @@
 package ru.myitschool.distspaceshooter;
 
-import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 import com.badlogic.gdx.math.Vector3;
-import com.badlogic.gdx.utils.ScreenUtils;
 
 public class MyGG extends Game {
 	public static final int SCR_WIDTH = 540, SCR_HEIGHT = 960;
@@ -18,8 +15,7 @@ public class MyGG extends Game {
 	SpriteBatch batch;
 	OrthographicCamera camera;
 	Vector3 touch;
-	BitmapFont font, fontLarge;
-	InputKeyboard keyboard;
+	BitmapFont fontSmall, fontLarge, fontTitle;
 
 	ScreenIntro screenIntro;
 	ScreenGame screenGame;
@@ -36,7 +32,6 @@ public class MyGG extends Game {
 		camera.setToOrtho(false, SCR_WIDTH, SCR_HEIGHT);
 		touch = new Vector3();
 		generateFont();
-		//keyboard = new InputKeyboard(SCR_WIDTH, SCR_HEIGHT, 8);
 
 		screenIntro = new ScreenIntro(this);
 		screenGame = new ScreenGame(this);
@@ -48,7 +43,7 @@ public class MyGG extends Game {
 	@Override
 	public void dispose () {
 		batch.dispose();
-		font.dispose();
+		fontSmall.dispose();
 		fontLarge.dispose();
 	}
 
@@ -56,14 +51,18 @@ public class MyGG extends Game {
 		FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal("crystal.ttf"));
 		FreeTypeFontGenerator.FreeTypeFontParameter parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
 		parameter.size = 30;
-		parameter.color = new Color().set(1, 0.9f, 0.3f, 1);
+		parameter.color = Color.valueOf("fff15a");
 		parameter.borderColor = Color.BLACK;
 		parameter.borderWidth = 2;
 		parameter.borderStraight = true;
 		parameter.characters = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяabcdefghijklmnopqrstuvwxyzАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789][_!$%#@|\\/?-+=()*&.;:,{}\"´`'<>";
-		font = generator.generateFont(parameter);
+		fontSmall = generator.generateFont(parameter);
 		parameter.size = 50;
 		fontLarge = generator.generateFont(parameter);
+		generator = new FreeTypeFontGenerator(Gdx.files.internal("dscrystal.ttf"));
+		parameter.size = 80;
+		parameter.color = Color.valueOf("be1616");
+		fontTitle = generator.generateFont(parameter);
 		generator.dispose();
 	}
 }
--- core/src/ru/myitschool/distspaceshooter/ScreenIntro.java
+++ core/src/ru/myitschool/distspaceshooter/ScreenIntro.java
@@ -1,7 +1,6 @@
 package ru.myitschool.distspaceshooter;
 
-import static ru.myitschool.distspaceshooter.MyGG.SCR_HEIGHT;
-import static ru.myitschool.distspaceshooter.MyGG.SCR_WIDTH;
+import static ru.myitschool.distspaceshooter.MyGG.*;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
@@ -9,18 +8,17 @@ import com.badlogic.gdx.graphics.Texture;
 
 public class ScreenIntro implements Screen {
     MyGG gg;
-
     Texture imgBG;
-
     TextButton btnPlay, btnSettings, btnAbout, btnExit;
+    String titleOfGame = "Space Shooter";
 
     public ScreenIntro(MyGG myGG){
         gg = myGG;
-        btnPlay = new TextButton(gg.fontLarge, "PLAY", 100, 600);
-        btnSettings = new TextButton(gg.fontLarge, "SETTINGS", 100, 500);
-        btnAbout = new TextButton(gg.fontLarge, "ABOUT", 100, 400);
-        btnExit = new TextButton(gg.fontLarge, "EXIT", 100, 300);
-        imgBG = new Texture("stars.png");
+        btnPlay = new TextButton(gg.fontLarge, "PLAY", 50, 400);
+        btnSettings = new TextButton(gg.fontLarge, "SETTINGS", 50, 300);
+        btnAbout = new TextButton(gg.fontLarge, "ABOUT", 50, 200);
+        btnExit = new TextButton(gg.fontLarge, "EXIT", 50, 100);
+        imgBG = new Texture("bg/space00.jpg");
     }
 
     @Override
@@ -30,7 +28,7 @@ public class ScreenIntro implements Screen {
 
     @Override
     public void render(float delta) {
-// обработка касаний
+        // обработка касаний
         if(Gdx.input.justTouched()) {
             gg.touch.set(Gdx.input.getX(), Gdx.input.getY(), 0);
             gg.camera.unproject(gg.touch);
@@ -48,11 +46,14 @@ public class ScreenIntro implements Screen {
             }
         }
 
+        // игровые события
+
         // отрисовка графики
         gg.camera.update();
         gg.batch.setProjectionMatrix(gg.camera.combined);
         gg.batch.begin();
         gg.batch.draw(imgBG, 0, 0, SCR_WIDTH, SCR_HEIGHT);
+        gg.fontTitle.draw(gg.batch, titleOfGame, 20, 860);
         btnPlay.font.draw(gg.batch, btnPlay.text, btnPlay.x, btnPlay.y);
         btnSettings.font.draw(gg.batch, btnSettings.text, btnSettings.x, btnSettings.y);
         btnAbout.font.draw(gg.batch, btnAbout.text, btnAbout.x, btnAbout.y);
